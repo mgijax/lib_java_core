b79feb1f2ef3c77e4455a044598751262d09605b
mbw <>
2004-01-28 20:47:08 +0000

    formating only changes

--- java/org/jax/mgi/shr/config/RecordStampCfg.java
+++ java/org/jax/mgi/shr/config/RecordStampCfg.java
@@ -1,7 +1,6 @@
 package org.jax.mgi.shr.config;
 
 import java.util.HashMap;
-
 import org.jax.mgi.shr.cache.FullCachedLookup;
 import org.jax.mgi.shr.cache.CacheException;
 import org.jax.mgi.shr.cache.KeyValue;
@@ -23,187 +22,175 @@ import org.jax.mgi.dbs.SchemaConstants;
  * @author M. Walker
  * @version 1.0
  */
+public class RecordStampCfg
+    extends Configurator {
+    /**
+     * a cache of job stream names and keys that have been previously looked up
+     */
+    private static HashMap cache = new HashMap();
+    /*
+     * the following are constants for exceptions thrown by this class
+     */
+    private static String UserNotFound =
+        ConfigExceptionFactory.UserNotFound;
+    /**
+         * default constructor which obtains a reference to the ConfigurationManager
+     * singleton class.
+     * @assumes nothing
+     * @effects nothing
+     * @throws ConfigException
+     */
+    public RecordStampCfg() throws ConfigException {
+        super();
+    }
 
-public class RecordStampCfg extends Configurator
-{
-	/**
-	 * a cache of job stream names and keys that have been previously looked up
-	 */
-	private static HashMap cache = new HashMap();
+    /**
+     * get the string which is used as the value of the created_by field
+     * and modified_by field when performing automatic record stamping either
+     * through the java bcp frameworks or dao frameworks.
+     * The parameter name read from the configuration file is JOBSTREAM.
+     * The default value is the user.name java System property.
+     * @assumes nothing
+     * @effects nothing
+     * @return string to be used in the created_by database record field.
+     */
+    public String getJobStreamName() {
+        return getConfigString("JOBSTREAM", System.getProperty("user.name"));
+    }
 
-	/*
-	 * the following are constants for exceptions thrown by this class
-	 */
-	private static String UserNotFound =
-			ConfigExceptionFactory.UserNotFound;
+    /**
+     * get the job stream key used for record stamping.
+     * The parameter name read from the configuration file is JOBKEY.
+     * @assumes nothing
+     * @effects nothing
+     * @return string to be used in the created_by database record field.
+     * @throws ConfigException thrown if there is an error accessing the
+     * configuration or if the JOBKEY parameter is not found
+     */
+    public Integer getJobKey() throws ConfigException {
+        return getConfigInteger("JOBKEY");
+    }
 
-	/**
-	 * default constructor which obtains a reference to the ConfigurationManager
-	 * singleton class.
-	 * @assumes nothing
-	 * @effects nothing
-	 * @throws ConfigException
-	 */
-	public RecordStampCfg() throws ConfigException {
-		super();
-	}
+    /**
+     * lookup the job stream key using the configured job stream name
+     * @assumes nothing
+     * @effects nothing
+     * @return the job stream key
+     * @throws ConfigException thrown if the name could not be found
+     * or there was an error accessing the database or configuration file
+     */
+    public Integer getJobStreamKey() throws ConfigException {
+        String name = getJobStreamName();
+        UserLookup lookup = null;
+        /**
+         * first check the cache
+         */
+        Integer key = (Integer) cache.get(name);
+        if (key == null) {
+            try {
+                lookup = new UserLookup();
+                key = lookup.lookupByName(name);
+            }
+            catch (MGIException e) {
+                ConfigExceptionFactory eFactory = new ConfigExceptionFactory();
+                ConfigException e2 = (ConfigException)
+                    eFactory.getException(UserNotFound, e);
+                e2.bind(name);
+                throw e2;
+            }
+            if (key == null) {
+                ConfigExceptionFactory eFactory = new ConfigExceptionFactory();
+                ConfigException e2 = (ConfigException)
+                    eFactory.getException(UserNotFound);
+                e2.bind(name);
+                throw e2;
+            }
+        }
+        /**
+         * store results in cache and return the value
+         */
+        cache.put(name, key);
+        return key;
+    }
 
-	/**
-	 * get the string which is used as the value of the created_by field
-	 * and modified_by field when performing automatic record stamping either
-	 * through the java bcp frameworks or dao frameworks.
-	 * The parameter name read from the configuration file is JOBSTREAM.
-	 * The default value is the user.name java System property.
-	 * @assumes nothing
-	 * @effects nothing
-	 * @return string to be used in the created_by database record field.
-	 */
-	public String getJobStreamName()
-	{
-		return getConfigString("JOBSTREAM", System.getProperty("user.name"));
-	}
+    /**
+     * get the string which is used as the value of the modified_by field when
+         * performing automatic record stamping with the bcp or dao java frameworks.
+     * The parameter name read from the configuration file is MODIFIED_BY.
+     * The default value is the user.name java System property.
+     * @return string to be used in the modified_by database record field.
+     */
+    public String getModifiedBy() {
+        String value =
+            getConfigString("MODIFIED_BY", System.getProperty("user.name"));
+        return value;
+    }
 
-	/**
-	 * get the job stream key used for record stamping.
-	 * The parameter name read from the configuration file is JOBKEY.
-	 * @assumes nothing
-	 * @effects nothing
-	 * @return string to be used in the created_by database record field.
-	 * @throws ConfigException thrown if there is an error accessing the
-	 * configuration or if the JOBKEY parameter is not found
-	 */
-	public Integer getJobKey() throws ConfigException
-	{
-		return getConfigInteger("JOBKEY");
-	}
+    /**
+     * get the string which is used as the value of the created_by field when
+         * performing automatic record stamping with the bcp or dao java frameworks.
+     * The parameter name read from the configuration file is CREATED_BY.
+     * The default value is the user.name java System property.
+     * @return string to be used in the created_by database record field.
+     */
+    public String getCreatedBy() {
+        String value =
+            getConfigString("CREATED_BY", System.getProperty("user.name"));
+        return value;
+    }
 
-	/**
-	 * lookup the job stream key using the configured job stream name
-	 * @assumes nothing
-	 * @effects nothing
-	 * @return the job stream key
-	 * @throws ConfigException thrown if the name could not be found
-	 * or there was an error accessing the database or configuration file
-	 */
-	public Integer getJobStreamKey() throws ConfigException
-	{
-		String name = getJobStreamName();
-		UserLookup lookup = null;
-		/**
-		 * first check the cache
-		 */
-		Integer key = (Integer)cache.get(name);
-		if (key == null)
-		{
-			try {
-				lookup = new UserLookup();
-				key = lookup.lookupByName(name);
-			}
-			catch (MGIException e) {
-				ConfigExceptionFactory eFactory = new ConfigExceptionFactory();
-				ConfigException e2 = (ConfigException)
-						eFactory.getException(UserNotFound, e);
-				e2.bind(name);
-				throw e2;
-			}
-			if (key == null) {
-				ConfigExceptionFactory eFactory = new ConfigExceptionFactory();
-				ConfigException e2 = (ConfigException)
-						eFactory.getException(UserNotFound);
-				e2.bind(name);
-				throw e2;
-			}
-		}
-		/**
-		 * store results in cache and return the value
-		 */
-		cache.put(name, key);
-		return key;
-	}
+    public class UserLookup
+        extends FullCachedLookup {
+        // internal cache
+        private HashMap cache = new HashMap();
+        /**
+         * the default constructor
+         * @assumes nothing
+         * @effects nothing
+         * @throws ConfigException thrown if there is an error accessing the
+         * configuration file
+             * @throws DBException thrown if there is an error accessing the database
+             * @throws CacheException thrown if there is an error accessing the cache
+         */
+        public UserLookup() throws ConfigException, DBException, CacheException {
+            super(SQLDataManagerFactory.getShared(SchemaConstants.MGD));
+        }
 
-	/**
-	 * get the string which is used as the value of the modified_by field when
-	 * performing automatic record stamping with the bcp or dao java frameworks.
-	 * The parameter name read from the configuration file is MODIFIED_BY.
-	 * The default value is the user.name java System property.
-	 * @return string to be used in the modified_by database record field.
-	 */
-	public String getModifiedBy() {
-		String value =
-				getConfigString("MODIFIED_BY", System.getProperty("user.name"));
-		return value;
-	}
+        /**
+         * look up a userid for the given user name from the MGI_USER table
+         * @param name the name to lookup
+         * @assumes nothing
+         * @effects nothing
+         * @return the userid
+         */
+        public Integer lookupByName(String name) throws DBException,
+            CacheException, KeyNotFoundException {
+            return (Integer) lookup(name);
+        }
 
-	/**
-	 * get the string which is used as the value of the created_by field when
-	 * performing automatic record stamping with the bcp or dao java frameworks.
-	 * The parameter name read from the configuration file is CREATED_BY.
-	 * The default value is the user.name java System property.
-	 * @return string to be used in the created_by database record field.
-	 */
-	public String getCreatedBy() {
-		String value =
-				getConfigString("CREATED_BY", System.getProperty("user.name"));
-		return value;
-	}
+        /**
+         * get the query to fully initialize the cache
+         * @assumes nothing
+         * @effects nothing
+         * @return the query to fully initialize the cache
+         */
+        public String getFullInitQuery() {
+            return new String("SELECT _User_key, login FROM MGI_USER");
+        }
 
-	public class UserLookup extends FullCachedLookup
-	{
-		// internal cache
-		private HashMap cache = new HashMap();
+        /**
+         * get a RowDataInterpreter for creating
+         * @return the RowDataInterpreter
+         */
+        public RowDataInterpreter getRowDataInterpreter() {
+            class Interpreter
+                implements RowDataInterpreter {
+                public Object interpret(RowReference row) throws DBException {
+                    return new KeyValue(row.getString(2), row.getInt(1));
+                }
+            }
 
-		/**
-		 * the default constructor
-		 * @assumes nothing
-		 * @effects nothing
-		 * @throws ConfigException thrown if there is an error accessing the
-		 * configuration file
-		 * @throws DBException thrown if there is an error accessing the database
-		 * @throws CacheException thrown if there is an error accessing the cache
-		 */
-		public UserLookup() throws ConfigException, DBException, CacheException
-		{
-			super(SQLDataManagerFactory.getShared(SchemaConstants.MGD));
-		}
-
-		/**
-		 * look up a userid for the given user name from the MGI_USER table
-		 * @param name the name to lookup
-		 * @assumes nothing
-		 * @effects nothing
-		 * @return the userid
-		 */
-		public Integer lookupByName(String name)
-		throws DBException, CacheException, KeyNotFoundException
-		{
-			return (Integer)lookup(name);
-		}
-
-		/**
-		 * get the query to fully initialize the cache
-		 * @assumes nothing
-		 * @effects nothing
-		 * @return the query to fully initialize the cache
-		 */
-		public String getFullInitQuery()
-		{
-			return new String("SELECT _User_key, login FROM MGI_USER");
-		}
-
-		/**
-		 * get a RowDataInterpreter for creating
-		 * @return the RowDataInterpreter
-		 */
-		public RowDataInterpreter getRowDataInterpreter()
-		{
-			class Interpreter implements RowDataInterpreter {
-				public Object interpret(RowReference row) throws DBException {
-					return new KeyValue(row.getString(2), row.getInt(1));
-				}
-			}
-			return new Interpreter();
-		}
-
-	}
-
+            return new Interpreter();
+        }
+    }
 }
