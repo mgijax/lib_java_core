0cdfe4474d25443d3bac79c54b3514dc432a090e
Jonathan Beal <Jon.Beal@jax.org>
2004-02-10 16:10:23 +0000

    Added many more definitions

--- java/org/jax/mgi/shr/dto/DTOConstants.java
+++ java/org/jax/mgi/shr/dto/DTOConstants.java
@@ -49,6 +49,13 @@ public class DTOConstants
     public static String Synonyms = "synonyms";
     public static String Aliases = "aliases";
 
+    public static String NomenEvents = "nomenEvents";
+    public static String OldName = "oldName";
+    public static String NewName = "newName";
+    public static String OldSymbol = "oldSymbol";
+    public static String Event = "event";
+    public static String EventDate = "eventDate";
+
     // alleles -- constants dealing with allele information
 
     public static String AlleleCounts = "alleleCounts";
@@ -84,6 +91,7 @@ public class DTOConstants
     public static String LastReference = "lastRef";
     public static String ReferenceCount = "refCount";
     public static String RefsKey = "refsKey";
+    public static String RefsKeys = "refsKeys";
     public static String RefsTitle = "refsTitle";
 
     // gene ontology -- constants dealing with GO data
@@ -121,6 +129,16 @@ public class DTOConstants
     public static String SeqID = "seqID";
     public static String SeqIDs = "seqIDs";
     public static String LogicalDbKey = "logicalDbKey";
+    public static String InterProTerms = "interProTerms";
+
+    // Sequences -- constants dealing with sequence data
+
+    public static String Sequences = "sequences";
+    public static String SequenceKey = "sequenceKey";
+    public static String SequenceLength = "length";
+    public static String SequenceType = "seqType";
+    public static String Strain = "strain";
+    public static String RawStrain = "rawStrain";
 
     // URLs -- constants dealing with URLs
 
@@ -134,15 +152,20 @@ public class DTOConstants
     public static String EvidenceCode = "evidenceCode";
     public static String InferredFrom = "inferredFrom";
     public static String TermKey = "_Term_key";
+    public static String Term = "term";
 
     // miscellaneous -- constants which fit none of the other sections
 
+    public static String DatabaseVersion = "databaseVersion";
     public static String DatabaseDate = "databaseDate";
     public static String Notes = "notes";
 }
 
 /*
 * $Log$
+* Revision 1.1  2003/12/30 16:56:28  mbw
+* imported into this product
+*
 * Revision 1.2  2003/12/01 13:10:21  jsb
 * Code review changes, added some new terms
 *
