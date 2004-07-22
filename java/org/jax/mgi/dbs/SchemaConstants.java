f7c44abf54713b3aa6937d99136eab85237cab6e
mbw <>
2004-07-22 18:35:39 +0000

    added IMSR

--- java/org/jax/mgi/dbs/SchemaConstants.java
+++ java/org/jax/mgi/dbs/SchemaConstants.java
@@ -1,18 +1,18 @@
 package org.jax.mgi.dbs;
 
 /**
- * @is An object that contains constant definitions for known db schemas.
+ * An object that contains constant definitions for known db schemas.
  * @has constant definitions
  * @does nothing
  * @company The Jackson Laboratory
  * @author mbw
- * @version 1.0
  */
 
 public class SchemaConstants
 {
-		// Constants
-		//
-		public static final String MGD = "MGD";
-		public static final String RADAR = "RADAR";
+        // Constants
+        //
+        public static final String MGD = "MGD";
+        public static final String RADAR = "RADAR";
+        public static final String IMSR = "IMSR";
 }
