6d01c52256586bb8a59f03495079cfdd1dabaf3a
mbw <>
2004-10-11 16:01:02 +0000

    fixed to suit changes in target code

--- test/org/jax/mgi/shr/dbutils/TestScriptWriter.java
+++ test/org/jax/mgi/shr/dbutils/TestScriptWriter.java
@@ -1,31 +1,30 @@
-/*
- * Created on Jan 22, 2004
- *
- * To change the template for this generated file go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
 package org.jax.mgi.shr.dbutils;
 
 import junit.framework.TestCase;
 
 import java.util.Vector;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.File;
+
 
 import org.jax.mgi.shr.dbutils.dao.DAO;
 import org.jax.mgi.shr.dbutils.dao.Script_Stream;
 import org.jax.mgi.shr.exception.MGIException;
+import org.jax.mgi.shr.unitTest.*;
 
-/**
- * @author mbw
- *
- * To change the template for this generated type comment go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
 public class TestScriptWriter extends TestCase
 {
 
     ScriptWriter writer = null;
     SQLDataManager sqlMgr = null;
     Script_Stream stream = null;
+    TableCreator tc = null;
+    FileUtility fileUtil = null;
+    String sybaseOutFile = "test" + File.separator + "SybaseErrorFile.txt";
+    String sybaseOutCompare = "test" + File.separator + "SybaseErrorFile.compare";
+
 
     /**
      * Constructor for TestScriptWriter.
@@ -36,55 +35,117 @@ public class TestScriptWriter extends TestCase
         super(arg0);
     }
 
-    /*
-     * @see TestCase#setUp()
-     */
     protected void setUp() throws Exception
     {
         super.setUp();
         sqlMgr = new SQLDataManager();
-        writer = new ScriptWriter(sqlMgr);
-        stream = new Script_Stream(writer);
+        tc = new TableCreator(sqlMgr.getUrl(), sqlMgr.getDatabase(),
+                              sqlMgr.getUser(), sqlMgr.getPassword(),
+                              sqlMgr.getConnectionManagerClass());
+        tc.createDBsimple();
     }
 
-    /*
-     * @see TestCase#tearDown()
-     */
     protected void tearDown() throws Exception
     {
         sqlMgr = null;
         writer = null;
         stream = null;
+        fileUtil.delete("script.out");
+        fileUtil.delete("script.sql");
+        fileUtil.delete("script_1.out");
+        fileUtil.delete("script_1.sql");
+        tc.dropDBsimple();
+        tc = null;
         super.tearDown();
     }
 
     public void testWrite() throws Exception
     {
+        writer = new ScriptWriter(sqlMgr);
+        writer.setOkToOverwrite(true);
+        stream = new Script_Stream(writer);
         SimpleDAO dao = new SimpleDAO();
-        //writer.write("This is string one");
-        //writer.write("This is string one");
-        //writer.go();
-        //writer.execute();
         stream.insert(dao);
         stream.close();
+        BufferedReader in = new BufferedReader(new FileReader("script.sql"));
+        String str;
+        str = in.readLine();
+        assertEquals("insert into Test_DBSimple values (1, 'one')", str);
+        str = in.readLine();
+        assertEquals("go", str);
+        in.close();
+        BufferedReader in2 = new BufferedReader(new FileReader("script.out"));
+        str = in2.readLine();
+        assertEquals("1> insert into Test_DBSimple values (1, 'one')", str);
+        str = in2.readLine();
+        assertEquals("(1 row affected)", str);
+        in2.close();
     }
+
+    public void testOkToOverwrite() throws Exception
+    {
+        writer = new ScriptWriter(sqlMgr);
+        writer.setOkToOverwrite(true);
+        stream = new Script_Stream(writer);
+        ScriptWriter writer2 = new ScriptWriter(sqlMgr);
+        writer2.setOkToOverwrite(false);
+        Script_Stream stream2 = new Script_Stream(writer2);
+        SimpleDAO dao = new SimpleDAO();
+        stream.insert(dao);
+        stream2.insert(dao);
+        stream.close();
+        stream2.close();
+        assertTrue(fileUtil.doesExist("script_1.out"));
+        assertTrue(fileUtil.doesExist("script_1.sql"));
+    }
+
+    public void testOkToOverwrite2() throws Exception
+    {
+        writer = new ScriptWriter(sqlMgr);
+        writer.setOkToOverwrite(true);
+        stream = new Script_Stream(writer);
+        ScriptWriter writer2 = new ScriptWriter(sqlMgr);
+        writer2.setOkToOverwrite(true);
+        Script_Stream stream2 = new Script_Stream(writer2);
+        SimpleDAO dao = new SimpleDAO();
+        stream.insert(dao);
+        stream2.insert(dao);
+        stream.close();
+        stream2.close();
+        assertTrue(!fileUtil.doesExist("script_1.out"));
+        assertTrue(!fileUtil.doesExist("script_1.sql"));
+    }
+
+
+    public void testErrorSummary() throws Exception
+    {
+        ScriptWriter.SybaseOutFile outFile =
+            ScriptWriter.getSybaseOutFile(sybaseOutFile);
+        String summary = (String)outFile.parseForErrors();
+        System.out.println(summary);
+        fileUtil.createFile("results", summary);
+        assertTrue(fileUtil.compare("results", sybaseOutCompare));
+        fileUtil.delete("results");
+    }
+
 
     public class SimpleDAO extends DAO
     {
 
         public String getUpdateSQL()
         {
-            return new String("Update dao with this string");
+            return new String("update Test_DBSimple set columnB = 'uno' " +
+                              "where columnA = 1");
         }
 
         public String getInsertSQL()
         {
-            return new String("Insert dao with this string");
+            return new String("insert into Test_DBSimple values (1, 'one')");
         }
 
         public String getDeleteSQL()
         {
-            return new String("Delete dao with this string");
+            return new String("delete TestDBSimple where columnA = 1");
         }
 
         public Vector getBCPVector(Table table)
@@ -100,3 +161,4 @@ public class TestScriptWriter extends TestCase
     }
 
 }
+
