9ae47e73ca969d81da38be181fcb63842e895e17
mbw <>
2005-08-01 15:40:09 +0000

    Changed interface parameter to SVASet

--- java/org/jax/mgi/shr/bucketizer/Decider.java
+++ java/org/jax/mgi/shr/bucketizer/Decider.java
@@ -1,6 +1,11 @@
+// $HEADER$
+// $NAME$
+
 package org.jax.mgi.shr.bucketizer;
 
 import java.util.Hashtable;
+
+import org.jax.mgi.shr.sva.SVASet;
 
 /**
  *
@@ -8,15 +13,20 @@ import java.util.Hashtable;
  * truely related to each other. The AbstractBucketizer has a Decider which it
  * calls when it derives that the two objects are related and will allow an
  * implementation to accept or decline the association. A default Decider is
- * used by the AbstractBucketizer which always returns true.
+ * used by the AbstractBucketizer if none is provided by the application
+ * which always returns true.
  * @has nothing
- * @does returns an object (defined by the implementation) if true else returns
- * null if false.
+ * @does returns an object (defined by the implementation) if true else
+ * returns null if false.
  * @company Jackson Laboratory
  * @author M Walker
  *
  */
 
 public interface Decider {
-    Object decide(Bucketizable setMember1, Bucketizable setMember2, Hashtable sva);
+    Object decide(Bucketizable setMember1, Bucketizable setMember2,
+                  SVASet svaSet);
 }
+
+// $LOG$
+
