584af6780f28415d70aaeec6b65aa3252d55385f
mbw <>
2005-08-11 16:38:30 +0000

    javadocs only

--- java/org/jax/mgi/shr/dbutils/QuerySeries.java
+++ java/org/jax/mgi/shr/dbutils/QuerySeries.java
@@ -2,6 +2,14 @@ package org.jax.mgi.shr.dbutils;
 
 import java.util.*;
 
+/**
+ * Is an object which stores multiple queries
+ * @has a series of sql statements
+ * @does provides an iterator over the execution of the series
+ * @company The Jackson Laboratory
+ * @author M Walker
+ */
+
 public class QuerySeries
 {
 
@@ -11,6 +19,14 @@ public class QuerySeries
     private ResultsNavigator closeNav = null;
 
 
+    /**
+     * constructor
+     * @assumes nothing
+     * @effects new object created
+     * @param sqlStatements a collection of sql statements as strings
+     * @param sqlMgr the SQLDataManager to use for querying
+     */
+
     protected QuerySeries(ArrayList sqlStatements,
                           SQLDataManager sqlMgr)
     {
@@ -19,16 +35,34 @@ public class QuerySeries
         this.sqlMgr = sqlMgr;
     }
 
+    /**
+     * gets the count of statements
+     * @assumes nothing
+     * @effects nothing
+     * @return count of statements
+     */
     public int queryCount()
     {
         return sqlStatements.size();
     }
 
+    /**
+     * get whether or not there are more statements to execute
+     * @assumes nothing
+     * @effects nothing
+     * @return true if there are more statements to execute, false otherwise
+     */
     public boolean hasNext()
     {
         return i.hasNext();
     }
 
+    /**
+     * execute the next query in the series
+     * @assumes nothing
+     * @effects nothing
+     * @return a ResultsNavigator for the results from the next query
+     */
     public ResultsNavigator executeNextQuery()
     throws DBException
     {
